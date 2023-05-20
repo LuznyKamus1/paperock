a9170c6c027a94214e541bb6c1f537b8d6103dd4
kamus <kamus@pop-os.localdomain>
2023-05-20 23:12:45 +0200

    made it more user friendly but less pretty

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,40 @@ use casual;
 use rand::prelude::*;
 
 fn main() {
-    let player: usize;
-    let computer: usize;
+    let mut player: usize;
+    let mut computer: usize;
     let array = ["papier", "kamien", "nozyce"];
+    let mut wynik = 0;
+    let mut input: String;
+    loop{
+       
+        input = casual::prompt("wybierz: papier/kamien/nozyce: ").get();
+        player = match input.as_str(){
+            "papier"=>0,
+            "kamien"=>1,
+            "nozyce"=>2,
+            _=>panic!("zla odpowiedz!")
+        };
+        println!("wybrales: {}", array[player]);
 
-    println!("papier - 0");
-    println!("kamien - 1");
-    println!("nozyce - 2");
-    
-    player = casual::prompt(": ").get();
-    println!("wybrales: {}", array[player]);
+        computer = rand::thread_rng().gen_range(0..3);
+        println!("komputer wybral: {}", array[computer]);
 
-    computer = rand::thread_rng().gen_range(0..3);
-    println!("komputer wybral: {}", array[computer]);
-
-    let result = game(player,computer);
-    match result{
-        0=>println!("you won!"),
-        1=>println!("you lost!"),
-        2=>println!("draw!"),
-        _=>panic!("wrong resoult")
+        let result = game(player,computer);
+        match result{
+            0=>(println!("wygrales"), wynik=wynik+1),
+            1=>(println!("przegrales"), wynik=wynik-1),
+            2=>(println!("remis"), ()),
+            _=>panic!("zly wynik")
+        };
+        println!("twoj wynik to: {}", wynik);
+        input = casual::prompt("czy chcesz zagrac ponownie - tak/nie :").get();
+        match input.as_str(){
+            "tak"=>println!("ok!"),
+            _=>break,
+        }
     }
+    println!("nara")
 }
 fn game(p: usize, c: usize) -> usize {
 
